28f368cdb0540f3a3568c546557bb696e19322b6
Jeffeffe3 <Jeffeffe3@users.noreply.github.com>
2021-06-14 21:24:19 +0200

    push 2
    added func

---
+++ prog7311/prog7311/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prog7311.Models
+{
+    public partial class Employee
+    {
+        public int ItemID { get; set; }
+
+        public int  quantity { get; set; }
+
+        public DateTime time { get; set; }
+
+    }
+}
